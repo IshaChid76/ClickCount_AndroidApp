62224cfb9ca5c8e28c3122e9289dec9d88c326da
IshaChid76 <ishachidrawar77@gmail.com>
2019-04-25 23:42:16 +0530

    commit

--- app/src/main/java/com/example/user/clickcount/MainActivity.java
+++ app/src/main/java/com/example/user/clickcount/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.user.clickcount;
 
+import android.content.Context;
+import android.content.res.Configuration;
 import android.icu.text.DisplayContext;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -7,43 +9,67 @@ import android.view.Display;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    static final String SELECTED_ITEM_POSITION = "ItemPosition";
+    static final String SELECTED_ITEM_POSITION = "Count";
+    static final String getSelectedItemPosition = "BgCount";
 
     private Button Click;
     private TextView ButtonCount,BackgroundCount;
-    private int ButCount;
+    private int ButCount,BackCount;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
+        if (savedInstanceState != null) {
+            ButCount = savedInstanceState.getInt(SELECTED_ITEM_POSITION);
+            BackCount = savedInstanceState.getInt(getSelectedItemPosition);
+        }
+
+        setContentView(R.layout.activity_main);
         Click = (Button) findViewById(R.id.Click);
-        ButtonCount = (TextView) findViewById(R.id.ButtonCount);
-        BackgroundCount = (TextView) findViewById(R.id.BackgroundCount);
+        ButtonCount = (TextView) this.findViewById(R.id.ButtonCount);
+        BackgroundCount = (TextView) this.findViewById(R.id.BackgroundCount);
 
         Click.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 ButCount++;
-                ButtonCount.setText(Integer.toString(ButCount));
+                setView(MainActivity.this);
             }
         });
 
+
+    }
+
+    public void setView(Context ctx) {
+
+        ButtonCount.setText(Integer.toString(ButCount));
+        BackgroundCount.setText(Integer.toString(BackCount));
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        BackCount++;
+        setView(MainActivity.this);
     }
 
     protected void onSaveInstanceState(final Bundle outState){
         super.onSaveInstanceState(outState);
 
         outState.putInt(SELECTED_ITEM_POSITION,ButCount);
+        outState.putInt(getSelectedItemPosition,BackCount);
     }
 
     protected void onRestoreInstanceState(final Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
 
         ButCount = savedInstanceState.getInt(SELECTED_ITEM_POSITION);
-        ButtonCount.setText(Integer.toString(ButCount));
+        BackCount = savedInstanceState.getInt(getSelectedItemPosition);
+        setView(MainActivity.this);
     }
 }
